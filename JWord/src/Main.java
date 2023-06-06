69f7e6c89dd29ff7d34796e3285d27fd37fb19fe
Chidubem Orazulike <101337926+Chidubem-Degaulle-Orazulike@users.noreply.github.com>
2023-06-06 13:28:53 +0100

    Add files via upload

--- JWord/src/Main.java
+++ JWord/src/Main.java
@@ -19,12 +19,12 @@ public class Main {
     public static void main(String[] args) {
     //load in the two word lists
         try{
-            Scanner in_dict  = new Scanner(new File("gameDictionary.txt"));
+            Scanner in_dict  = new Scanner(new File("JWord/src/gameDictionary.txt"));
             while(in_dict.hasNext()){
                 dictionary.add(in_dict.next());
             }
 
-            Scanner in_targets = new Scanner(new File("targetWords.txt"));
+            Scanner in_targets = new Scanner(new File("JWord/src/targetWords.txt"));
             while(in_targets.hasNext()){
                 targetWords.add(in_targets.next());
                 rhymeHint.add(in_targets.next());
